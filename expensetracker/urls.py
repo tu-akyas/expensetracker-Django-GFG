442b93e3fd75ca7535703e47165b450e383c64d2
Akyas <>
2024-07-10 23:44:39 +0800

    Fix import in urls.py and fix css import in index.html

--- expensetracker/urls.py
+++ expensetracker/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls.conf import static
+from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
